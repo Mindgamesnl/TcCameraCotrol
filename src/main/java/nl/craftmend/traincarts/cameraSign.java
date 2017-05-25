8be2dcd572359f0f1b946bc6cebcdad196efe2b2
Mindgamesnl (Mats moolhuizen) <matsmoolhuizen@gmail.com>
2017-05-25 15:19:28 +0200

    Biem

--- src/main/java/nl/craftmend/traincarts/cameraSign.java
+++ src/main/java/nl/craftmend/traincarts/cameraSign.java
@@ -7,6 +7,7 @@ import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
 import com.bergerkiller.bukkit.tc.signactions.SignActionType;
+import nl.craftmend.TcCamControl;
 import nl.craftmend.screen.objects.playerScreen;
 import nl.craftmend.screenManager;
 import org.bukkit.Bukkit;
@@ -32,88 +33,88 @@ public class cameraSign extends SignAction {
                 if ((info.isAction(new SignActionType[]{SignActionType.REDSTONE_ON, SignActionType.GROUP_ENTER})) && (info.hasGroup())) {
                     for (MinecartMember<?> member : info.getGroup()) {
                         if (!((CommonMinecart) member.getEntity()).isEmpty()) {
-                            if (((CommonMinecart) member.getEntity()).getPassenger() instanceof Player) {
-                                if (info.getLine(2).equalsIgnoreCase("reset")) {
-
-                                } else {
-                                    screenManager.set(((CommonMinecart) member.getEntity()).getPlayerPassenger(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), ((CommonMinecart) member.getEntity()).getUniqueId());
-                                }
-                            } else {
-                                if (info.getLine(2).equalsIgnoreCase("reset")) {
-                                    for (playerScreen screen : screenManager.list.values()) {
-                                        if (screen.getId() == ((CommonMinecart) member.getEntity()).getUniqueId()) {
-                                            screenManager.reset(screen.getPlayer());
-
-                                        }
-                                    }
-                                }
-                                for (playerScreen screen : screenManager.list.values()) {
-                                    if (screen.getId() == ((CommonMinecart) member.getEntity()).getUniqueId()) {
-                                        screenManager.set(screen.getPlayer(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), screen.getId());
-                                    }
-                                }
-                            }
-
-
+                            prosess(member, info);
                         }
                     }
                 }
             } else if (info.isCartSign()) {
                 if ((info.isAction(new SignActionType[]{SignActionType.REDSTONE_ON, SignActionType.MEMBER_ENTER})) && (info.hasMember())) {
                     if (!((CommonMinecart) info.getMember().getEntity()).isEmpty()) {
-                        if (((CommonMinecart) info.getMember().getEntity()).getPassenger() instanceof Player) {
-                            if (info.getLine(2).equalsIgnoreCase("reset")) {
-
-                            } else {
-                                screenManager.set(((CommonMinecart) info.getMember().getEntity()).getPlayerPassenger(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), ((CommonMinecart) info.getMember().getEntity()).getUniqueId());
-                            }
-                        } else {
-                            if (info.getLine(2).equalsIgnoreCase("reset")) {
-                                for (playerScreen screen : screenManager.list.values()) {
-                                    if (screen.getId() == ((CommonMinecart) info.getMember().getEntity()).getUniqueId()) {
-                                        screenManager.reset(screen.getPlayer());
-
-                                    }
-                                }
-                            }
-                            for (playerScreen screen : screenManager.list.values()) {
-                                if (screen.getId() == ((CommonMinecart) info.getMember().getEntity()).getUniqueId()) {
-                                    screenManager.set(screen.getPlayer(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), screen.getId());
-                                }
-                            }
-                        }
+                        prosess(info.getMember(), info);
                     }
                 }
             } else if (info.isRCSign()) {
                 if (info.isAction(new SignActionType[]{SignActionType.REDSTONE_ON})) {
                     for (MinecartMember<?> member : info.getGroup()) {
                         if (!((CommonMinecart) member.getEntity()).isEmpty()) {
-                            if (((CommonMinecart) member.getEntity()).getPassenger() instanceof Player) {
-                                if (info.getLine(2).equalsIgnoreCase("reset")) {
-
-                                } else {
-                                    screenManager.set(((CommonMinecart) member.getEntity()).getPlayerPassenger(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), ((CommonMinecart) member.getEntity()).getUniqueId());
-                                }
-                            } else {
-                                if (info.getLine(2).equalsIgnoreCase("reset")) {
-                                    for (playerScreen screen : screenManager.list.values()) {
-                                        if (screen.getId() == ((CommonMinecart) info.getMember().getEntity()).getUniqueId()) {
-                                            screenManager.reset(screen.getPlayer());
-
-                                        }
-                                    }
-                                }
-                                for (playerScreen screen : screenManager.list.values()) {
-                                    if (screen.getId() == ((CommonMinecart) member.getEntity()).getUniqueId()) {
-                                        screenManager.set(screen.getPlayer(), Float.parseFloat(info.getLine(2)), Float.parseFloat(info.getLine(3)), screen.getId());
-                                    }
-                                }
-                            }
+                            prosess(member, info);
                         }
                     }
                 }
             }
         }
+    }
+
+    public void prosess(MinecartMember<?> member, SignActionEvent info) {
+
+        if (info.getLine(2).equalsIgnoreCase("smooth")) {
+
+            if (((CommonMinecart) member.getEntity()).getPassenger() instanceof Player) {
+
+                Integer stepsFin = 0;
+
+                Float startYaw = ((CommonMinecart) ((CommonMinecart) member.getEntity()).getPassenger()).getLocation().getYaw();
+                Float startPitch = ((CommonMinecart) ((CommonMinecart) member.getEntity()).getPassenger()).getLocation().getYaw();
+
+                Float targetYaw = Float.parseFloat(info.getLine(2).split(":")[0]);
+                Float targetPitch = Float.parseFloat(info.getLine(2).split(":")[1]);
+
+                Float relativeYaw = startYaw - targetYaw;
+                Float relativePitch = startPitch - targetPitch;
+
+                Integer steps = Integer.parseInt(info.getLine(3));
+
+                Float stepsYaw = relativeYaw / steps;
+                Float stepsPitch = relativePitch / steps;
+
+                Bukkit.getScheduler().scheduleAsyncRepeatingTask(TcCamControl.getPl(), new Runnable() {
+                    @Override
+                    public void run() {
+
+
+
+                    }
+                },0 ,2);
+            }
+
+        } else {
+            if (((CommonMinecart) member.getEntity()).getPassenger() instanceof Player) {
+                if (info.getLine(2).equalsIgnoreCase("reset")) {
+                } else {
+                    Float yaw = Float.parseFloat(info.getLine(2).split(":")[0]);
+                    Float pitch = Float.parseFloat(info.getLine(2).split(":")[1]);
+                    screenManager.set(((CommonMinecart) member.getEntity()).getPlayerPassenger(), yaw, pitch, ((CommonMinecart) member.getEntity()).getUniqueId());
+                }
+            } else {
+                if (info.getLine(2).equalsIgnoreCase("reset")) {
+                    for (playerScreen screen : screenManager.list.values()) {
+                        if (screen.getId() == ((CommonMinecart) info.getMember().getEntity()).getUniqueId()) {
+                            screenManager.reset(screen.getPlayer());
+
+                        }
+                    }
+                }
+                for (playerScreen screen : screenManager.list.values()) {
+                    if (screen.getId() == ((CommonMinecart) member.getEntity()).getUniqueId()) {
+                        Float yaw = Float.parseFloat(info.getLine(2).split(":")[0]);
+                        Float pitch = Float.parseFloat(info.getLine(2).split(":")[1]);
+                        screenManager.set(screen.getPlayer(), yaw, pitch, screen.getId());
+                    }
+                }
+            }
+        }
+
+
     }
 
     public boolean build(SignChangeActionEvent event)
